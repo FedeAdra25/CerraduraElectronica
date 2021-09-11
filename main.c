ba209974ecd294dc17e37e952ea49193e90850f8
FedeAdra25 <fedeadra@gmail.com>
2021-09-11 18:53:25 -0300

    Some changes

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ void delay_ms(uint16_t time);
 void delay_ms(uint16_t time){
 	volatile unsigned long l=0;
 	for(uint16_t i=0;i<time;i++)
-		for(l=0;l<9975;l++);
+		for(l=0;l<9000;l++);
 }
 
 int main(){
@@ -14,10 +14,9 @@ int main(){
 	GPIOC->CRH = 0x44344444; //PC13 as output
 	while(1){
 		GPIOC->ODR ^= (1<<13); //Toggle PC13
-		delay_ms(10000);
+		delay_ms(100);
 	}
 	
 	return 0;
 		
 }
-
