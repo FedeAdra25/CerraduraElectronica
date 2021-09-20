793defe04e695846ac94f66d30403d31c3aaebd2
FedeAdra25 <fedeadra@gmail.com>
2021-09-20 20:06:33 -0300

    papi fede arregla todo

--- Proteus/STM32F103C6/main.h
+++ Proteus/STM32F103C6/main.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * main.h
  *
@@ -24,30 +23,3 @@
 #endif /* MAIN_H_ */
 
 /* End of file */
-=======
-/*
- * main.h
- *
- * Created: 06-May-21 12:19:39 AM
- *  Author: fedea
- */ 
-
-
-#ifndef MAIN_H_
-#define MAIN_H_
-
-//DEFINO FRECUENCIA DEL CLOCK DEL MCU (en Hz)
-#define F_CPU 72000000 //72Mhz
-//delays segun frecuencia del clock
-#include "utils.h"
-
-
-//Tipos de datos enteros estandar
-#include <stdint.h>
-//Manejar caracteres
-#include <stdlib.h>
-
-#endif /* MAIN_H_ */
-
-/* End of file */
->>>>>>> de3a9183d6336055bac3317749efa36a66907361
--- Proteus/STM32F103C6/utils.c
+++ Proteus/STM32F103C6/utils.c
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-
-#include "utils.h"
-#ifndef F_CPU
-#define F_CPU 72000000UL
-#endif
-
-#define CONST_FOR_US_DELAY 12
-#define CONST_FOR_MS_DELAY 11998
-
-static unsigned long ms_delay_const = CONST_FOR_MS_DELAY * (72000000UL / F_CPU); 
-
-void delay_us(unsigned long amount){
-	unsigned long i,l;
-	for(i=0;i<amount;i++)
-		for(l=0;l<CONST_FOR_US_DELAY;l++);
-}
-
-void delay_ms(unsigned long amount){
-	unsigned long i,l;
-	for(i=0;i<amount;i++)
-		for(l=0;l<ms_delay_const;l++);
-}
-=======
-
 #include "utils.h"
 
 
@@ -40,4 +15,3 @@ void delay_ms(unsigned long amount){
 	for(i=0;i<amount;i++)
 		for(l=0;l<ms_delay_const;l++);
 }
->>>>>>> de3a9183d6336055bac3317749efa36a66907361
